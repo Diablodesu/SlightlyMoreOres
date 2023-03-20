a264dca64bb823134c4a4cb065b68a03a52f3914
Diablodesu <diablodevspam@gmail.com>
2023-03-20 07:39:41 -0400

    changed the way uranium kills the player

--- src/main/java/net/mcreator/slightlymoreores/procedures/UraniumInInventoryDeathProcedure.java
+++ src/main/java/net/mcreator/slightlymoreores/procedures/UraniumInInventoryDeathProcedure.java
@@ -2,12 +2,14 @@ package net.mcreator.slightlymoreores.procedures;
 
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 
 public class UraniumInInventoryDeathProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity)
-			_entity.setHealth(0);
+			_entity.addEffect(new MobEffectInstance(MobEffects.HARM, 90, 2, (false), (false)));
 	}
 }
