147131291244a6e2f8215885a82500edf6b17660
Diablodesu <diablodevyt@gmail.com>
2023-09-18 21:54:50 -0400

    the funny (real)

--- src/main/java/dev/diablodesu/slightlymoreores/procedures/TheFunnyProcedure.java
+++ src/main/java/dev/diablodesu/slightlymoreores/procedures/TheFunnyProcedure.java
@@ -1,11 +1,17 @@
 package dev.diablodesu.slightlymoreores.procedures;
 
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.Vec2;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
 
 public class TheFunnyProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		if (world instanceof Level _level && !_level.isClientSide())
-			_level.explode(null, x, y, z, (float) (0 / 0), Level.ExplosionInteraction.TNT);
+		if (world instanceof ServerLevel _level)
+			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
+					"minecraft:explosion ~ ~ ~ ~ ~ ~ 1 999999999 normal @p");
 	}
 }
