e4d81fdc234997ea50122a17c88472053aa9f378
Diablodesu <diablodevyt@gmail.com>
2023-09-18 21:58:20 -0400

    oh go fuck yourself

--- src/main/java/dev/diablodesu/slightlymoreores/procedures/TheFunnyProcedure.java
+++ src/main/java/dev/diablodesu/slightlymoreores/procedures/TheFunnyProcedure.java
@@ -12,6 +12,6 @@ public class TheFunnyProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		if (world instanceof ServerLevel _level)
 			_level.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, new Vec3(x, y, z), Vec2.ZERO, _level, 4, "", Component.literal(""), _level.getServer(), null).withSuppressedOutput(),
-					"minecraft:explosion ~ ~ ~ ~ ~ ~ 1 999999999 normal @p");
+					"particle minecraft:explosion ~ ~ ~ ~ ~ ~ 1 999999999 normal @p");
 	}
 }
